74ab2024fdb28fbe9b3645d546ce20c1ea59b0b9
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-07-09 15:16:31 +0530

    Created boilerplate

---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='w-full h-screen bg-zinc-300 flex gap-4 items-center justify-center'>
+      Card
+    </div>
+  )
+}
+
+export default Card
